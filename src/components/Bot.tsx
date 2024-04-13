d1ffab9a38d94c5c090bda602f9f39e12eb44576
Matthew Marcellino <mattmarclj@gmail.com>
2024-04-13 23:02:24 +0700

    update

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -979,8 +979,7 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
               />
             )}
           </div>
-          <div style={{ height: '8px' }} />
-          {/* <Badge badgeBackgroundColor={props.badgeBackgroundColor} poweredByTextColor={props.poweredByTextColor} botContainer={botContainer} /> */}
+          <Badge badgeBackgroundColor={props.badgeBackgroundColor} poweredByTextColor={props.poweredByTextColor} botContainer={botContainer} />
         </div>
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
